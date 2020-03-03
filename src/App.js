6292ecdd14b2ab09e4d5058709b873e60404ea7f
Argosa <ronald.kratochwill@live.com>
2020-03-03 16:47:49 -0700

    Props Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
-import HelloWorld from './components/HelloWorld'
+import PersonCard from './components/PersonCard'
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld />
+      <PersonCard firstName='Bill' lastName='Clinton' age='73' hairColor='Grey'/>
+      <PersonCard firstName='Barack' lastName='Obama' age='58' hairColor='Brown'/>
+      <PersonCard firstName='George' lastName='Bush' age='73' hairColor='Grey'/>
+      <PersonCard firstName='Theodore' lastName='Roosevelt' age='55' hairColor='Brown'/>
     </div>
   );
 }
---
+++ src/components/PersonCard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class PersonCard extends Component {
+    render() {
+        const {firstName, lastName, age, hairColor} = this.props;
+        return (
+            <div>
+                <h1>{firstName} {lastName}</h1>
+                <h4>Age: {age}</h4>
+                <h4>Hair Color: {hairColor}</h4>
+            </div>
+        );
+        
+    }
+}
+
+export default PersonCard
