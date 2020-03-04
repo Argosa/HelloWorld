a8dded4f07a20dac7fc2aa4e1005b5cbbe1d95f2
Argosa <ronald.kratochwill@live.com>
2020-03-03 17:28:41 -0700

    add birthday button

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 
+const Button = (props) => {
+    return (
+    <button onClick={props.handleclick}>Birthday Button for {props.firstName} {props.lastName}</button>
+    );
+}
+
 class PersonCard extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            addCount: 0
+        }
+    }
+
+    handleclick = () => {
+        this.setState({
+            addCount: this.state.addCount + 1
+        })
+    }
+
     render() {
         const {firstName, lastName, age, hairColor} = this.props;
+        let newAge = parseInt(age) + this.state.addCount
         return (
             <div>
                 <h1>{firstName} {lastName}</h1>
-                <h4>Age: {age}</h4>
+                <h4>Age: {newAge}</h4>
                 <h4>Hair Color: {hairColor}</h4>
+                <Button handleclick={this.handleclick} count={this.state.addCount}/>
             </div>
         );
         
